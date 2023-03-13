71630d5321e3db4a8210a8c6d4f339f9b537a342
aliaydogdu105 <aliaydogdu105@gmail.com>
2023-03-13 05:53:02 +0300

    feat: add blog_serializer-view

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import Category, Blog
+from .serializers import CategorySerializer, BlogSerializer
+
+
+class CategoryView(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class BlogView(ModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
