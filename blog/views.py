e20fc01169b89141e4811030c74f829d2437cc85
aliaydogdu105 <aliaydogdu105@gmail.com>
2023-03-14 02:58:32 +0300

    feat: add permissions

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,20 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Category, Blog
 from .serializers import CategorySerializer, BlogSerializer
+from .permissions import IsAdminOrReadOnly
 
 
 class CategoryView(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
     filterset_fields = ['name']
     
 
 class BlogView(ModelViewSet):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
     filterset_fields = ['category']
     search_fields = ['title', 'content']
